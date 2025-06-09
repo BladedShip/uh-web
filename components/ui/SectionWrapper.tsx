b5d437cbd3a59a3929c96073739233c51fd2766a
BladedShip <ultimateallyn@gmail.com>
2025-06-09 16:40:39 +0530

    Add classnames dependency and refactor SectionWrapper to use utility function for class names

--- components/ui/SectionWrapper.tsx
+++ components/ui/SectionWrapper.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
+import { cn } from "@/utils";
 
 type Props = {
     id: string;
     children: React.ReactNode;
     className?: string;
-}
+};
 
 const SectionWrapper = ({ id, children, className }: Props) => {
-  return (
-    <section id={id} className={`h-[80vh] md:h-screen w-full relative p-2 py-4 ${className}`}>
-        {children}
-    </section>
-  )
-}
+    return (
+        <section id={id} className={cn(`h-[80vh] md:h-screen w-full relative p-2 py-4`, className)}>
+            {children}
+        </section>
+    );
+};
 
-export default SectionWrapper
+export default SectionWrapper;
